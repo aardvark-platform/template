99549bbf888a406bf11e30420c65188a84680d48
krauthaufen <krauthaufen@awx.at>
2015-07-23 09:43:54 +0200

    updated build script;

--- build.cmd
+++ build.cmd
@@ -9,16 +9,17 @@ IF exist boot.fsx (
 del "boot.fsx"
 RD Packages /S /Q
 ) ELSE ( 
+
 IF exist packages\FAKE ( echo skipping FAKE download ) ELSE ( 
 echo downloading FAKE
 REM dir
-"bin\nuget.exe" "install" "FAKE" "-OutputDirectory" "Packages" "-ExcludeVersion" "-Prerelease"
-"bin\nuget.exe" "install" "FSharp.Formatting.CommandTool" "-OutputDirectory" "Packages" "-ExcludeVersion" "-Prerelease"
+"bin\nuget.exe" "install" "FAKE" "-Version" "3.35.2" "-OutputDirectory" "Packages" "-ExcludeVersion"
+"bin\nuget.exe" "install" "FSharp.Formatting.CommandTool" "-OutputDirectory" "Packages" "-ExcludeVersion"
 "bin\nuget.exe" "install" "SourceLink.Fake" "-OutputDirectory" "Packages" "-ExcludeVersion"
 "bin\nuget.exe" "install" "NUnit.Runners" "-OutputDirectory" "Packages" "-ExcludeVersion"
 "bin\nuget.exe" "install" "Aardvark.Build" "-OutputDirectory" "Packages" "-ExcludeVersion"
-"bin\nuget.exe" "install" "Paket.Core" "-OutputDirectory" "packages" "-ExcludeVersion"
-))
+"bin\nuget.exe" "install" "Paket.Core" "-Version" "1.18.5" "-OutputDirectory" "packages" "-ExcludeVersion"
+)
 
 SET TARGET=Default
 IF NOT [%1]==[] (set TARGET=%1)
@@ -28,8 +29,9 @@ SET /P t=<tmp
 SETLOCAL EnableDelayedExpansion
 IF DEFINED t SET "t=!t:%1 =!"
 SET args=!t!
+del tmp
 
 "packages\FAKE\tools\Fake.exe" "build.fsx" "target=%TARGET%" %args%
-del tmp
+
 )
 
