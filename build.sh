772deb5a8ac51a7b80b6bd30141ec55c0a426b02
krauthaufen <krauthaufen@awx.at>
2015-09-24 12:54:06 +0200

    fixed build.sh;

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DIR=dirname "$0"
+pushd $DIR
+
 if [ ! -d "packages/FAKE" ]; then
 	echo "downloading FAKE"
 	mono --runtime=v4.0 bin/nuget.exe install FAKE -OutputDirectory packages -Version 3.35.2 -ExcludeVersion
@@ -9,15 +12,5 @@ if [ ! -d "packages/FAKE" ]; then
 	mono --runtime=v4.0 bin/nuget.exe install Paket.Core -OutputDirectory packages -Version 1.18.5 -ExcludeVersion 
 fi
 
-
-if [ ! -d "\\\\hobel\\NuGet\\" ]; then
-	echo "attempting to mount hobel"
-	sudo mkdir "\\\\hobel\\NuGet\\"
-	sudo chmod 777 "\\\\hobel\\NuGet\\"
-	echo "please enter your VRVis username"
-	read user
-	echo "mounting hobel"
-	sudo mount -t cifs -o user=$user,dom=VRVIS,uid=$(id -u),gid=$(id -u) //hobel.ra1.vrvis.lan/NuGet "\\\\hobel\\NuGet\\"
-fi
 
 mono packages/FAKE/tools/FAKE.exe "build.fsx"  $@
