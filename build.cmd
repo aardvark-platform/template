2932803033f075ad7a7640a537658f2a13bcf923
krauthaufen <krauthaufen@awx.at>
2018-11-13 11:56:22 +0100

    sync

--- build.cmd
+++ build.cmd
@@ -1,23 +1,37 @@
 @echo off
-SETLOCAL
+setlocal enableextensions enabledelayedexpansion
 PUSHD %~dp0
 
-ECHO bootstrapping package manager ...
 .paket\paket.bootstrapper.exe
 if errorlevel 1 (
   exit /b %errorlevel%
 )
 
-SET FSI_PATH=packages\build\FAKE\tools\Fake.exe
-
-IF exist boot.fsx ( 
-    "%FSHARPINSTALLDIR%fsi.exe" "boot.fsx" 
+IF exist boot.fsx (
+    powershell write-host -fore Green bootstrapping project 
+    SET VSPATH=''
+    for /f "delims=" %%A in ('.\.paket\vswhere.exe -property installationPath') do set "VSPATH=%%A"
+    SET "fsi=!VSPATH!\Common7\IDE\CommonExtensions\Microsoft\FSharp\fsi.exe"
+    
+    IF NOT exist "!fsi!" (
+        SET "fsi=%FSHARPINSTALLDIR%\fsi.exe"
+        IF NOT exist "!fsi!" (
+            powershell write-host -fore Red fsi.exe not found. please install fsharp tools or Visual Studio
+            exit /b 1
+        )   
+    )
+    
+    "!fsi!" "boot.fsx" 
+    if errorlevel 1 (
+      exit /b %errorlevel%
+    )
     del "boot.fsx"
-	.paket\paket.exe install
+    del ".\.paket\vswhere.exe"
+    .paket\paket.exe install
 )
 
 if NOT exist paket.lock (
-	echo No paket.lock found, running paket install.
+	powershell write-host -fore Yellow  No paket.lock found, running paket install.
 	.paket\paket.exe install
 )
 
@@ -26,7 +40,7 @@ if errorlevel 1 (
   exit /b %errorlevel%
 )
 
-"%FSI_PATH%" "build.fsx" Dummy --fsiargs build.fsx %* 
+"packages\build\FAKE\tools\Fake.exe" "build.fsx" Dummy --fsiargs build.fsx %* 
 
 
 
