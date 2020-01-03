f22c0e467fc5079d941e138a38ad9e225729c5ac
krauthaufen <krauthaufen@awx.at>
2020-01-03 15:52:35 +0100

    fixed build.sh

--- build.fsx
+++ build.fsx
@@ -6,22 +6,10 @@ open System
 open System.IO
 open System.Diagnostics
 open Aardvark.Fake
-open Fake.Core
-open Fake.Core.TargetOperators
-open Fake.DotNet
 
 do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
 DefaultSetup.install ["src/__SOLUTION_NAME__.sln"]
 
-Target.create "Start" (fun _ ->
-    let param (p : DotNet.Options) =
-        { p with WorkingDirectory = Path.Combine("bin", "Release", "netcoreapp2.0") }
-
-    DotNet.exec param "" "__PROJECT_NAME__.dll" |> ignore
-)
-
-Target.create "Run" (fun _ -> Target.run 1 "Start" [])
-"Compile" ==> "Run"
 
 entry()
--- build.sh
+++ build.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-if [ ! -f .paket/paket.exe ]; then
-    echo "run bootstrapper"
-    mono .paket/paket.bootstrapper.exe
+if [ ! -f ".paket/paket" ]; then
+	echo installing paket
+	dotnet tool install Paket --tool-path .paket
 fi
 
-if [ ! -f .paket/nuget.exe ]; then
-	echo "download nuget"
-	wget -P .paket https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+
+
+if [ -f boot.fsx ]; then
+	echo running boot
+	dotnet tool install fake-cli --tool-path .fakecli --version 5.16.0
+	.fakecli/fake run boot.fsx
+	rm boot.fsx
+	rm -dfr .fakecli
 fi
 
 if [ ! -f paket.lock ]; then
-	mono .paket/paket.exe install
+	.paket/paket install
 fi
 
-if [ -f boot.fsx ]; then
-	mono .paket/paket.exe restore
-        mono .paket/nuget.exe install FAKE -ExcludeVersion -OutputDirectory .paket -NonInteractive -Verbosity quiet
-        mono .paket/FAKE/tools/FAKE.exe --removeLegacyFakeWarning run boot.fsx
-	rm boot.fsx
-	mono .paket/paket.exe install
-fi
+.paket/paket restore
+dotnet packages/build/fake-cli/tools/netcoreapp2.1/any/fake-cli.dll build $@
 
 
-mono packages/build/FAKE/tools/FAKE.exe "build.fsx" Dummy --fsiargs build.fsx $@
